f8ec2556c2e3b085011f8f473ddf8dab52741703
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-10-02 02:57:48 +0300

    mini blog with the use of route(path) redirection

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 def index():
-    return {'data': {'name': 'Home Page'}}
+    return {'data': {'name': 'Blog Page'}}
 
 
-@app.get("/about")
-def about():
-    return {'data': {'name': 'About Page'}}
+@app.get('/blog/{id}')
+def blog(id: int):
+    return {'data': {'name': 'Blog Page', 'id': id}}
+
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'data': {'name': 'Blog Page', 'id': id, 'comments': ['comment1', 'comment2']}}
+
+
+@app.get("/{path:path}")
+async def redirect_to_root(path: str):
+    return RedirectResponse(url="/")
