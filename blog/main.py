9f785fddbcaaeff5f982e30b1e3e0527fa27a655
Muhammad Zaki <mahmadzekoo@gmail.com>
2023-10-02 17:24:15 +0300

    Status code v3

--- blog/main.py
+++ blog/main.py
@@ -29,6 +29,9 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
 @app.get('/blog', status_code=status.HTTP_200_OK)
 def get_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
+    if not blogs:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='No blogs found')
     return blogs
 
 
@@ -37,7 +40,7 @@ def get_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='Blog not found')
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
     return blog.first()
 
 
@@ -45,7 +48,8 @@ def get_blog(id: int, db: Session = Depends(get_db)):
 def delete_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
-        return {'detail': 'Blog not found'}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
     blog.delete(synchronize_session=False)
     db.commit()
     return {'detail': 'Blog deleted'}
@@ -55,7 +59,8 @@ def delete_blog(id: int, db: Session = Depends(get_db)):
 def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
-        return {'detail': 'Blog not found'}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
     blog.update({'title': request.title, 'content': request.content,
                 'published': request.published})
     db.commit()
